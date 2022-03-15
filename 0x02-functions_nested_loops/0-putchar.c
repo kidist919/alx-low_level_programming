95bc347796deb1fd9fbf7d3feedd5eded1d3ec2c
kidist919 <kidistm421@gmail.com>
2022-03-15 11:50:49 -0700

    print 0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "putchar.h"
+/**
+ * main - Prints _Putchar followed by newline
+ *
+ * Return: always to zero
+*/
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
